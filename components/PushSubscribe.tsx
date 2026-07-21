defb46889b5aa722c5ebb337db5ac6dd0449f264
zexlymp-droid <zexlymp@gmail.com>
2026-07-21 22:04:43 +0700

    Update PushSubscribe.tsx

--- components/PushSubscribe.tsx
+++ components/PushSubscribe.tsx
@@ -8,7 +8,11 @@ function urlBase64ToUint8Array(base64String: string) {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
   const rawData = atob(base64);
-  return Uint8Array.from([...rawData].map((c) => c.charCodeAt(0)));
+  const output = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; i++) {
+    output[i] = rawData.charCodeAt(i);
+  }
+  return output;
 }
 
 export default function PushSubscribe() {
